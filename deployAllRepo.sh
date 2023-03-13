4f373911150d80deeb92fcf1b321c9d11fec35b5
Vijaylakshmi Subramanian <vijaylakshmi.subramanian@andromeda360.ai>
2023-03-13 19:42:01 +0530

    Updated service url

--- deployAllRepo.sh
+++ deployAllRepo.sh
@@ -4,38 +4,40 @@ CONFIG_FILE="./.microcfg"
 CURRENT_DIR=$(pwd)
 
 source $CONFIG_FILE
+prefix="micro-shop"
 
 function build_image() {  
     docker build -t $1:latest -f docker/$1.Dockerfile .
     k3d image import --cluster micro-shop-local $1:latest  
 }
-
-declare -a gitrepos=("micro-shop-orders" "micro-shop-api" "micro-shop-products")
+prefix="micro-shop"
+#declare -a gitrepos=("orders" "api" "products")
+declare -a gitrepos=("api" "products")
 
 for repo in ${gitrepos[@]}; do
     echo    
     cd $MICRO_SERVICE_ROOT    
     echo "${repo}.........................................................."
-    if [ -d ${repo} ]; then
-        cd ${repo}
+    if [ -d ${prefix}-${repo} ]; then
+        cd ${prefix}-${repo}
         if [ "$(git fetch && git rev-list main...origin/main --count)" -gt "0" ]; then
             echo "Main branch behind origin/main."
             # git checkout main
             # git pull               
             git checkout main
             git pull
-            build_image ${repo}      
+            build_image ${prefix}-${repo}
         else
             echo "Main branch current or ahead of origin/main."
-            build_image ${repo}
+            build_image ${prefix}-${repo}
         fi
   else
     echo "Cloning repo."
-    git clone git@github.com:amitsatpute-gslab/${repo}.git
+    git clone git@github.com:amitsatpute-gslab/${prefix}-${repo}.git
     # git checkout main    
-    cd ${repo}
+    cd ${prefix}-${repo}
     git checkout dev
-    build_image ${repo}    
+    build_image ${prefix}-${repo}`
   fi
 
 done
